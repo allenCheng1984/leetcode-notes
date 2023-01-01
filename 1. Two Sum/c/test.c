b7e6d37afe83f5ed09d928188d44dfb0c98451de
Allen Cheng <allen.cheng1984@gmail.com>
2023-01-01 17:55:48 +0800

    Adjust Makefile options, refactor coding style and fix warning

--- 1. Two Sum/c/test.c
+++ 1. Two Sum/c/test.c
@@ -7,6 +7,7 @@ int main()
 {
     printf("\n== Test start ==\n");
     int numsSize;
+    int target;
     int *returnSize = (int *) malloc(sizeof(int));
 
     /*
@@ -15,39 +16,32 @@ int main()
         Output: [0,1]
         Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
      */
-    numsSize = 4;
-    int *nums1;
-    nums1 = (int *) malloc(numsSize * sizeof(int));
+    numsSize = 4, target = 9, *returnSize = 0;
     int data1[] = {2, 7, 11, 15};
-    for (size_t i = 0;i < numsSize;i++)
+    int *nums1 = (int *) malloc(numsSize * sizeof(int));
+    for (int i = 0;i < numsSize;i++)
         nums1[i] = data1[i];
 
-    int *case1 = twoSum(nums1, numsSize, 9, returnSize);
+    int *case1 = twoSum(nums1, numsSize, target, returnSize);
     assert(case1[0] == 0);
     assert(case1[1] == 1);
     assert(*returnSize == 2);
-    free(nums1);
-    *returnSize = 0;
-
 
     /*
         # Example 2:
         Input: nums = [3,2,4], target = 6
         Output: [1,2]
      */
-    numsSize = 3;
-    int *nums2;
-    nums2 = (int *) malloc(numsSize * sizeof(int));
+    numsSize = 3, target = 6, *returnSize = 0;
+    int *nums2 = (int *) malloc(numsSize * sizeof(int));
     int data2[] = {3, 2, 4};
-    for (size_t i = 0;i < numsSize;i++)
+    for (int i = 0;i < numsSize;i++)
         nums2[i] = data2[i];
 
-    int *case2 = twoSum(nums2, numsSize, 6, returnSize);
+    int *case2 = twoSum(nums2, numsSize, target, returnSize);
     assert(case2[0] == 1);
     assert(case2[1] == 2);
     assert(*returnSize == 2);
-    free(nums2);
-    *returnSize = 0;
 
 
     /*
@@ -55,19 +49,25 @@ int main()
         Input: nums = [3,3], target = 6
         Output: [0,1]
      */
-    numsSize = 2;
-    int *nums3;
-    nums3 = (int *) malloc(numsSize * sizeof(int));
+    numsSize = 2, target = 6, *returnSize = 0;
+    int *nums3 = (int *) malloc(numsSize * sizeof(int));
     int data3[] = {3, 3};
-    for (size_t i = 0;i < numsSize;i++)
+    for (int i = 0;i < numsSize;i++)
         nums3[i] = data3[i];
 
     int *case3 = twoSum(nums3, numsSize, 6, returnSize);
     assert(case3[0] == 0);
     assert(case3[1] == 1);
     assert(*returnSize == 2);
+
+    /* free all alloced memory */
+    free(nums1);
+    free(nums2);
     free(nums3);
-    *returnSize = 0;
+    free(returnSize);
+    free(case1);
+    free(case2);
+    free(case3);
 
     printf("== Test finish ==\n");
 }
--- 1. Two Sum/c/two_sum.c
+++ 1. Two Sum/c/two_sum.c
@@ -1,49 +1,49 @@
-#include "../../libs/uthash.h"
+#include "uthash.h"
 
 typedef struct {
-  int key;   // for hash table's key
-  int index; // for index of nums
-  UT_hash_handle hh;
+	int key;   // for hash table's key
+	int index; // for index of nums
+	UT_hash_handle hh;
 } hash_t;
 
 void hash_free(hash_t *head) {
-  hash_t *current, *temp;
+	hash_t *current, *temp;
 
-  HASH_ITER(hh, head, current, temp) {
-    HASH_DEL(head, current);
-    free(current);
-  }
+	HASH_ITER(hh, head, current, temp) {
+		HASH_DEL(head, current);
+		free(current);
+	}
 }
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-  hash_t *head = NULL;
-  hash_t *element;
-  int *result = (int*) malloc(2 * sizeof(int));
-  int remaining;
-  for (int i = 0; i < numsSize; ++i) {
-    remaining = target - nums[i];
+	hash_t *head = NULL;
+	hash_t *element;
+	int *result = (int*) malloc(2 * sizeof(int));
+	int remaining;
+	for (int i = 0; i < numsSize; ++i) {
+		remaining = target - nums[i];
 
-    // Find if there has already been an element such that the sum is target
-    HASH_FIND_INT(head, &remaining, element);
-    if (element) {
-      result[0] = element->index;
-      result[1] = i;
-      break;
-    }
+		// Find if there has already been an element such that the sum is target
+		HASH_FIND_INT(head, &remaining, element);
+		if (element) {
+			result[0] = element->index;
+			result[1] = i;
+			break;
+		}
 
-    // Add the new number to the hash head if it doesn't exist already
-    HASH_FIND_INT(head, &nums[i], element);
-    if (!element) {
-      element = (hash_t *) malloc(sizeof(*element));
-      element->key = nums[i];
-      element->index = i;
+		// Add the new number to the hash head if it doesn't exist already
+		HASH_FIND_INT(head, &nums[i], element);
+		if (!element) {
+			element = (hash_t *) malloc(sizeof(*element));
+			element->key = nums[i];
+			element->index = i;
 
-      HASH_ADD_INT(head, key, element);
-    }
-  }
+			HASH_ADD_INT(head, key, element);
+		}
+	}
 
-  hash_free(head);
+	hash_free(head);
 
-  *returnSize = 2;
-  return result;
+	*returnSize = 2;
+	return result;
 }
